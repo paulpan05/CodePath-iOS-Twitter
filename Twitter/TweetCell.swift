81b42e4d13d6717c43b3e1d0ab4ef43f62a31f0f
Paul Pan <panjunhong05@gmail.com>
2020-02-13 02:19:44 -0800

    Project completion

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,29 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Paul Pan on 20/2/13.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
